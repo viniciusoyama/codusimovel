1ae1f4bf819da704a3443eb647a7662619958fb2
Vinícius Oyama <vinicius.oyama@gmail.com>
2014-01-11 21:28:39 -0200

    created loop for properties

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
-var app = angular.module("myapp", ["firebase"]);
-function MyController($scope, $firebase) {
-  var ref = new Firebase("https://1389469963926.firebaseio.com/messages");
-  $scope.messages = $firebase(ref);
+var app = angular.module("CodusApp", ["firebase"]);
+
+app.controller("PropertiesController", [ "$scope", "$firebase" , function($scope, $firebase) {
+  var propertiesRef = new Firebase("https://1389469963926.firebaseio.com/propeties");
+  $scope.properties = $firebase(propertiesRef);
+  $scope.propertyStatuses = ['Pendente visita', 'Visitado', 'OK', 'Descartado'];
   $scope.addMessage = function(e) {
     if (e.keyCode != 13) return;
     $scope.messages.$add({from: $scope.name, body: $scope.msg});
     $scope.msg = "";
   }
-}
+}]);

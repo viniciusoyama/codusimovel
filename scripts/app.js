364ec2c983733e4a58db86eb270936724ef95402
Vinícius Oyama <vinicius.oyama@gmail.com>
2014-01-12 00:11:07 -0200

    configured property gcrud

--- scripts/app.js
+++ scripts/app.js
@@ -2,10 +2,20 @@ var app = angular.module("CodusApp", ["firebase"]);
 
 app.controller("PropertiesController", [ "$scope", "$firebase", "$window" , function($scope, $firebase, $window) {
   var propertiesRef = new Firebase("https://1389469963926.firebaseio.com/propeties");
-  var propertyStatuesRef = new Firebase("https://1389469963926.firebaseio.com/propeties");
+  var propertyStatuesRef = new Firebase("https://1389469963926.firebaseio.com/propertyStatuses");
   $scope.properties = $firebase(propertiesRef);
   $scope.propertyStatuses = $firebase(propertyStatuesRef);
-  $scope.newProperty = {status: 'Alou'};
+  $scope.newProperty = { status: ''};
+
+  function resetNewProperty() {
+    var keys = $scope.propertyStatuses.$getIndex();
+
+    keys.forEach(function(key, i) {
+      if (i == 0) {
+        $scope.newProperty = {status: $scope.propertyStatuses.$child(key).name};
+      }
+    });
+  }
 
   $scope.addPropertyStatus = function() {
     var newPropertyStatusName = $scope.newPropertyStatusName.trim();
@@ -24,8 +34,8 @@ app.controller("PropertiesController", [ "$scope", "$firebase", "$window" , func
 
   $scope.addProperty = function() {
 
-    $scope.propertyStatuses.$add($scope.newProperty);
-    $scope.newProperty = {status: 'Alou'};
+    $scope.properties.$add(formatPropertyToBeSaved($scope.newProperty));
+    resetNewProperty();
   };
 
   $scope.removeProperty = function(id) {
@@ -33,6 +43,16 @@ app.controller("PropertiesController", [ "$scope", "$firebase", "$window" , func
       $scope.properties.$remove(id);
     }
   };
+
+  function formatPropertyToBeSaved(property) {
+    property.rent = parseFloat(property.rent);
+    property.condominium = parseFloat(property.condominium);
+    property.iptu = parseFloat(property.iptu);
+    property.total = property.rent + property.condominium + property.iptu;
+
+    property.totalPerM2 = property.total/parseFloat(property.size);
+    return property;
+  }
 
 
 }]);

d9b8db765c8940fd18ef021bebc9ec59c7a260f7
Vinícius Oyama <vinicius.oyama@gmail.com>
2014-01-11 22:33:13 -0200

    created crud of statuses

--- app.js
+++ app.js
@@ -2,8 +2,26 @@ var app = angular.module("CodusApp", ["firebase"]);
 
 app.controller("PropertiesController", [ "$scope", "$firebase" , function($scope, $firebase) {
   var propertiesRef = new Firebase("https://1389469963926.firebaseio.com/propeties");
+  var propertyStatuesRef = new Firebase("https://1389469963926.firebaseio.com/propeties");
   $scope.properties = $firebase(propertiesRef);
-  $scope.propertyStatuses = ['Pendente visita', 'Visitado', 'OK', 'Descartado'];
+  $scope.propertyStatuses = $firebase(propertyStatuesRef);
+
+  $scope.addPropertyStatus = function () {
+    var newPropertyStatusName = $scope.newPropertyStatusName.trim();
+    if (!newPropertyStatusName.length) {
+      return;
+    }
+    $scope.propertyStatuses.$add({
+      name: newPropertyStatusName,
+      completed: false
+    });
+    $scope.newPropertyStatusName = '';
+  };
+
+  $scope.removePropertyStatus = function (id) {
+    $scope.propertyStatuses.$remove(id);
+  };
+
   $scope.addMessage = function(e) {
     if (e.keyCode != 13) return;
     $scope.messages.$add({from: $scope.name, body: $scope.msg});
